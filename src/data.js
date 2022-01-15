26228bf7abdb4029b3be643d21ca5f6e26a7a645
aman-deep1 <aman_deep@thbs.com>
2022-01-15 21:17:40 +0530

    commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Laptop',
+      price: 199999,
+      image: 'https://www.aptronixindia.com/media/catalog/product/1/3/13inchmacbookprowithtouchba2.0ghzr512gbspacegrey.jpeg',
+    },
+    {
+      id: '2',
+      name: 'Iphone',
+      price: 79000,
+      image: 'https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg',
+    },
+    {
+      id: '3',
+      name: 'Jacket',
+      price: 1500,
+      image: 'https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB1htB.aKL2gK0jSZFmq6A7iXXaf%2FMANTLCONX-Newest-Solid-Autumn-Mens-Jackets-Male-Casual-Zipper-Summer-Jacket-Men-Spring-Casual-Outwear-Men.jpg',
+    },
+    
+  ],
+};
+export default data;
